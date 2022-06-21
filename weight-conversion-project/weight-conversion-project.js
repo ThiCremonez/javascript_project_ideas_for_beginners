64e95a745f45ff2b274a49afe841b7dc9b629460
Thiago Cremonez <thiagocremonez@gmail.com>
2022-06-21 13:46:32 -0300

    changing html and css weight-converter

---
+++ weight-conversion-project/weight-conversion-project.js
@@ -0,0 +1,13 @@
+// this code transform the type chose in "mg" and it's converted to the others
+
+// transforming in mg
+
+var num_choose = document.querySelector('#num_choose');
+var unit = document.querySelector('#unit');
+
+if unit == {
+
+}
+
+// transforming in others units
+
